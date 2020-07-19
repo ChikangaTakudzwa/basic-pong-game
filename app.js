87724fbb422a0f670504c68f9f6129cefec9aad8
Francis Bukenya <frankie.bukenya@gmail.com>
2020-07-19 12:16:51 +0200

    Add start button and instructions

--- app.js
+++ app.js
@@ -202,13 +202,16 @@ class Pong {
 }
 
 const canvas = document.querySelector('#pong');
+const startGame = document.querySelector('.start-btn');
 const pong = new Pong(canvas);
 
-canvas.addEventListener('click', () => pong.play());
+
+
+startGame.addEventListener('click', () => pong.play());
 
 canvas.addEventListener('mousemove', (event) => {
   const scale = event.offsetY / event.target.getBoundingClientRect().height;
   pong.players[0].pos.y = canvas.height * scale;
 });
 
-pong.start();
+ pong.start();
